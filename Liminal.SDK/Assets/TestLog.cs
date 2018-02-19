5009a7ea116157d4a3ab298a29ce782e066d9c18
tinny <tinnystudios@gmail.com>
2018-02-20 09:47:02 +1100

    Stable Commit.
    This commits a stable version of the liminal platform sdk with the controller issue fixed.
    
    - Still need to check for experience app visiblity with experience loader.

---
+++ Liminal.SDK/Assets/TestLog.cs
@@ -0,0 +1,12 @@
+﻿using Liminal.SDK.VR.Avatars.Controllers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLog : MonoBehaviour {
+    public VRControllerVisual controllerVisual;
+
+    void OnEnable() {
+
+    }
+}

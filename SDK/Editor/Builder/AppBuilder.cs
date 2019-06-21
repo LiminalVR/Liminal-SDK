9364b333662859b96d35fa9abd2b14669b74740c
MichaelLiminal <42725479+MichaelLiminal@users.noreply.github.com>
2019-06-21 19:20:34 +1000

    Prevent system from including additional scenes in a limapp when building (#23)
    * Add code to force delete all asset bundles (even if they're in use) and then set the target scene's asset bundle to appscene. This prevents things like folders and textures from breaking the app builder script if they're accidentally set to an asset bundle and also prevents multiple scenes from being built to a single limapp.
    
    * Move comment to be above the code it's referring to.

--- SDK/Editor/Builder/AppBuilder.cs
+++ SDK/Editor/Builder/AppBuilder.cs
@@ -95,6 +95,15 @@ namespace Liminal.SDK.Editor.Build
             if (buildInfo == null)
                 throw new ArgumentNullException("buildInfo");
 
+            var assetBundles = AssetDatabase.GetAllAssetBundleNames();
+            foreach (var bundle in assetBundles)
+            {
+                // boolean true forces the asset bundles to be deleted even if they're in use.
+                AssetDatabase.RemoveAssetBundleName(bundle, true);
+            }
+
+            AssetImporter.GetAtPath(buildInfo.Scene.path).SetAssetBundleNameAndVariant("appscene", "");
+
             // Get and verify the target platform is supported
             var appPlatform = MapAppTargetPlatform(buildInfo.BuildTarget);
             if (appPlatform == AppPackPlatform.Unknown)

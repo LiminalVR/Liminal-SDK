c53f16adf473e8a403d053f86992de29921f8660
MichaelV <michael@liminalvr.com>
2019-09-11 14:00:41 +1000

    Add LWRP shaders to the package so that the system can automatically copy them into projects as needed. Required as we can't access Shaders from packages, only from projects.

---
+++ SDK/Editor/SDKResourcesConsts.cs
@@ -0,0 +1,17 @@
+﻿public static class SDKResourcesConsts
+{
+    /// <summary>
+    /// Path to Liminal SDK resources folder
+    /// </summary>
+    public const string LiminalSDKResourcesPath = "Assets/Liminal/Resources";
+
+    /// <summary>
+    /// Path to LWRP Shader resources folder
+    /// </summary>
+    public const string LWRPShaderResourcesPath = LiminalSDKResourcesPath + "/LightweightShaders";
+
+    /// <summary>
+    /// Path to package LWRP Shader resources folder
+    /// </summary>
+    public const string PackageLWRPShaders = "SDK/LightweightShaders";
+}
---
+++ SDK/Editor/Utils/LiminalSDKResources.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class LiminalSDKResources : EditorWindow
+{
+    [MenuItem("Examples/GUILayout TextField")]
+    static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(LiminalSDKResources));
+        window.Show();
+    }
+    void OnGUI()
+    {
+        if (GUILayout.Button("Test"))
+        {
+            SetupLightweightShaders();
+        }
+    }
+
+    private void SetupLightweightShaders()
+    {
+        if (!Directory.Exists(SDKResourcesConsts.LiminalSDKResourcesPath))
+            Directory.CreateDirectory(SDKResourcesConsts.LiminalSDKResourcesPath);
+
+        if (!Directory.Exists(SDKResourcesConsts.LWRPShaderResourcesPath))
+            Directory.CreateDirectory(SDKResourcesConsts.LWRPShaderResourcesPath);
+
+        var shadersPath = $"{UnityPackageManagerUtils.FullPackageLocation}{SDKResourcesConsts.PackageLWRPShaders}";
+
+        var files = Directory.GetFiles(shadersPath).Where(name => !name.EndsWith(".meta"));
+
+        foreach (var file in files)
+        {
+            File.Copy(file, SDKResourcesConsts.LWRPShaderResourcesPath + $"/{Path.GetFileName(file)}");
+        }
+
+
+        //check if shaders exist
+
+
+        //Debug.Log(shadersPath);
+        //File.Copy(shadersPath, BuildWindowConsts.PreviewAppScenePath);
+        AssetDatabase.Refresh();
+    }
+}

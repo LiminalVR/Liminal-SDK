de4cb5548ed9e656bd4fa807ea49b1603edfde27
Tin <tin@liminalvr.com>
2019-10-16 10:46:32 +1100

    Add another example.

---
+++ Examples/5.Badminton/Badminton/Scripts/Feeder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Feeder : MonoBehaviour
+{
+    public Shuttle ShuttlePrefab;
+    public Transform SpawnPoint;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            var shuttle = Instantiate(ShuttlePrefab, SpawnPoint.position, SpawnPoint.rotation);
+            shuttle.Rigidbody.velocity = SpawnPoint.forward * 11;
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
---
+++ Examples/5.Badminton/Badminton/Scripts/Racquet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Racquet : MonoBehaviour
+{
+    public Transform Target;
+    public Rigidbody RigidBody;
+
+    public Vector3 RotationOffset;
+
+    public void FixedUpdate()
+    {
+        RigidBody.MovePosition(Target.position);
+        RigidBody.MoveRotation(Target.rotation * Quaternion.Euler(RotationOffset));
+    }
+}

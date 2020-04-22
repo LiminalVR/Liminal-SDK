5f31d22bd1c664c61095b6a97589b5fcc340ca57
tinny <tinnystudios@gmail.com>
2020-04-22 14:25:16 +1000

    Fix error messages from build window

--- SDK/Editor/Builder/Windows/BuildSettingsWindow.cs
+++ SDK/Editor/Builder/Windows/BuildSettingsWindow.cs
@@ -112,7 +112,6 @@ namespace Liminal.SDK.Build
             if (!sceneExists)
             {
                 var scenePath = $"{UnityPackageManagerUtils.FullPackageLocation}/{BuildWindowConsts.PackagePreviewAppScenePath}";
-                Debug.Log(scenePath);
                 File.Copy(scenePath, BuildWindowConsts.PreviewAppScenePath);
                 AssetDatabase.Refresh();
             }

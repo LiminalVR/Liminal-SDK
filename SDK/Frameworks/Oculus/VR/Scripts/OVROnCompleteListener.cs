2d5bb9d19f64f255f60510e9b723884beb1c916c
tinny <tinnystudios@gmail.com>
2020-04-22 14:15:09 +1000

    Remove ovr listener

--- SDK/Frameworks/Oculus/VR/Scripts/OVROnCompleteListener.cs
+++ SDK/Frameworks/Oculus/VR/Scripts/OVROnCompleteListener.cs
@@ -16,18 +16,8 @@ permissions and limitations under the License.
 
 using UnityEngine;
 using System.Collections;
-
 #if UNITY_ANDROID && !UNITY_EDITOR
 
-public abstract class OVROnCompleteListener : AndroidJavaProxy
-{
-	public OVROnCompleteListener() : base("com.oculus.svclib.OnCompleteListener")
-	{
-	}
 
-	public abstract void onSuccess();
-
-	public abstract void onFailure();
-}
 
 #endif

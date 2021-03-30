417b974bb524a8a99a37e7c9aef1893753786bc0
tinny <tinnystudios@gmail.com>
2021-03-30 17:27:20 +1100

    Add some checks

---
+++ Examples/Scripts/FadeExample.cs
@@ -0,0 +1,17 @@
+﻿using Liminal.Core.Fader;
+using UnityEngine;
+
+public class FadeExample : MonoBehaviour
+{
+    public ScreenFader ScreenFader;
+
+    public void FadeToBlack()
+    {
+        ScreenFader.FadeToBlack();
+    }
+
+    public void FadeToClear()
+    {
+        ScreenFader.FadeToClear();
+    }
+}

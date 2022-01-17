e1818db893d15c88f5d24bdc110fd8a15c8da9e7
Tinny <tinnystudios@gmail.com>
2022-01-17 11:34:40 +1100

    Fix neo 2 in mirror reflection

--- SDK/Systems/Reflection System/Implementation/PlanarReflections/Scripts/MirrorReflection.cs
+++ SDK/Systems/Reflection System/Implementation/PlanarReflections/Scripts/MirrorReflection.cs
@@ -106,10 +106,19 @@ namespace App
 
             if (model == EDeviceModelType.Pico)
             {
+                /*
                 m_Renderer.material.SetFloat("_Quest", 0);
                 m_Renderer.material.SetFloat(s_offsetEnabled, 1);
                 m_Renderer.material.SetFloat("_Debug", 1);
                 m_Renderer.material.SetFloat("_OffsetX", 1);
+                */
+                m_Renderer.material.SetFloat("_OffsetRX", 1);
+                m_Renderer.material.SetFloat("_OffsetRY", 1);
+                m_Renderer.material.SetFloat("_OffsetRZ", 0);
+                m_Renderer.material.SetFloat("_OffsetRW", 0);
+                m_Renderer.material.SetFloat("_OffsetX", 1);
+
+                Debug.Log("Using Pico Neo 2 Values");
             }
 
             if (model == EDeviceModelType.PicoNeo3)
@@ -124,6 +133,8 @@ namespace App
                 m_Renderer.material.SetFloat("_OffsetRW", 0f);
 
                 m_Renderer.material.SetFloat("_OffsetX", 1);
+
+                Debug.Log("Using Pico Neo 3 Values");
             }
 
             void SetMaterial(ReflectionOffsetModel m)
